1aaa182d5ace2f9d6352cc42e705ef1fbcebe570
hemanshidobaria163 <54872963+hemanshidobaria163@users.noreply.github.com>
2021-10-26 20:17:25 -0700

    add shopping component and recipe component detail, created Ingredient model page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] =[
-    new Recipe('Banana-Bread','It is made with bananas', 'https://iambaker.net/wp-content/uploads/2011/09/banana-bread-5.jpg')
+    new Recipe('Banana-Bread','It is made with bananas', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F04%2F17%2F171761.jpg&q=85'),
+    new Recipe('Apple Pie','It is made with Apple', 'https://happymoneysaver.com/wp-content/uploads/2020/11/best-apple-pie-26-1.jpg'),
+    new Recipe('Fruit Tart', 'It is made with fresh fruits','https://www.culinaryhill.com/wp-content/uploads/2021/05/Fresh-Fruit-Tart-Culinary-Hill-hero.jpg')
   ];
   constructor() { }
 
