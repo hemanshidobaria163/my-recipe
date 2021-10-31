cb301613eadaa311fff69948e7190271ba9a493f
hemanshidobaria163 <54872963+hemanshidobaria163@users.noreply.github.com>
2021-10-31 15:01:37 -0700

    Adding navigatation with event binding, data with property binding, forms modul added and @vewchild decorator

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
   recipes: Recipe[] =[
     new Recipe('Banana-Bread','It is made with bananas', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F04%2F17%2F171761.jpg&q=85'),
     new Recipe('Apple Pie','It is made with Apple', 'https://happymoneysaver.com/wp-content/uploads/2020/11/best-apple-pie-26-1.jpg'),
@@ -16,5 +17,7 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
